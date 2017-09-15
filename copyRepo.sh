f603f2ee6985b7657f7f3dce599b3760bc9938e0
Michal Bryc <furchin@gmail.com>
2017-09-15 14:19:04 -0700

    Remove copyRepo which is unused

--- copyRepo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export USER=test
-git clone --bare git@github.com:builtforme/battleship.git && cd battleship.git && git push --mirror git@github.com:builtforme/battleship-${USER}.git && rm -rf battleship.git
