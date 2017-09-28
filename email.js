ce0049fb3ace20b732c0ed052c659b4209adcfe8
Michal Bryc <furchin@gmail.com>
2017-09-28 14:23:00 -0700

    Add function to notify for expiring invitations

--- email.js
+++ email.js
@@ -31,3 +31,29 @@ exports.sendRevocationNotification = (candidateName) => {
     });
   });
 };
+
+exports.sendInvitationExpiringNotification = (candidateNames) => {
+  return new Promise((resolve, reject) => {
+    ses.sendEmail({
+      Destination: {
+        ToAddresses: [process.env.REVOCATION_NOTIFICATION_RECEPIENT]
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: 'The following candidates have not started their assignment: ' + candidateNames
+          }
+        },
+        Subject: {
+          Data: `Project Invitations Expiring Within 24 Hours`
+        }
+      },
+      Source: process.env.EMAIL_FROM
+    }, function(err, data){
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
