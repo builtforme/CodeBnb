6fb3c9eada6d2a599b2cd53ed6b7b50e908bfa0a
Michal Bryc <furchin@gmail.com>
2018-06-21 17:32:06 -0700

    Emit actual usable endpoints

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,9 @@
-output "api_gw_base_url" {
-  value = "${aws_api_gateway_deployment.codeBnbApiGatewayDeployment.invoke_url}"
+output "AWS API Gateway Endpoint" {
+  value = "${aws_api_gateway_deployment.codeBnbApiGatewayDeployment.invoke_url}/addcandidate?authorization=${var.add_candidate_authorization_code}"
 }
 
-output "aws_api_gateway_domain_name" {
-  value = "${lower(var.lambda_function_name)}.${lower(var.domain)}"
+output "Custom Domain Endpoint (start here)" {
+  value = "https://${lower(var.lambda_function_name)}.${lower(var.domain)}/addcandidate?authorization=${var.add_candidate_authorization_code}"
 }
+
+#output ""
